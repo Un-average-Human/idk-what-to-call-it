3a9c02c54ce81bce488728ed026a94882a380608
Un-average-Human <miguelmfernandes1008@gmail.com>
2026-06-10 21:15:54 +1200

    more slight changes to the radial menu

--- airship-system/item_container.gd
+++ airship-system/item_container.gd
@@ -14,20 +14,22 @@ func _ready() -> void:
 	_on_focus_exited()
 
 	if item_resource:
-		if item_resource is Texture2D:
-			texture_normal = item_resource
-		elif item_resource and item_resource.texture:
+		if item_resource.texture:
 			texture_normal = item_resource.texture
 
 
 func _on_focus_entered():
 	var focus_tween = create_tween().set_parallel(true)
+	focus_tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+	
 	focus_tween.tween_property(self, "scale", Vector2(1.2, 1.2), animation_speed)
 	focus_tween.tween_property(self, "modulate", Color(1.5, 1.5, 1.5), animation_speed)
 	print("Mouse entered the container!")
 
 func _on_focus_exited():
 	var focus_tween = create_tween().set_parallel(true)
+	focus_tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
+	
 	focus_tween.tween_property(self, "scale", Vector2(1.0, 1.0), animation_speed)
 	focus_tween.tween_property(self, "modulate", Color(1, 1, 1), animation_speed)
 	print("Mouse left the container!")
